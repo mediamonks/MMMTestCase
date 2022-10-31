f184db3af55b50dd1b108efe290feec0461e8dc2
Aleh Dzenisiuk <aleh@mediamonks.com>
2022-10-31 10:19:02 +0100

    Add allCasesDictionary for MMMLoadableState
    This pairs with certain unit tests using varyParameters to go through all loadable states.

---
+++ Sources/MMMTestCase/MMMLoadableState.swift
@@ -0,0 +1,17 @@
+//
+// Starbucks App.
+// Copyright (c) 2022 MediaMonks. All rights reserved.
+// 
+
+import MMMLoadable
+
+extension MMMLoadableState {
+	/// A dictionary with all states suitable for `MMMTestCase.varyParameters(_:block:)`
+	/// (keys are kebab-case already).
+	public private(set) static var allCasesDictionary: [String: MMMLoadableState] = [
+		"idle": .idle,
+		"syncing": .syncing,
+		"did-fail-to-sync": .didFailToSync,
+		"did-sync-successfully": .didSyncSuccessfully
+	]
+}
